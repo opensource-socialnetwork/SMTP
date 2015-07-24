c9a43eb76df57f38e0f23892de285f000caf90f5
Arsalan Shah <manactric@gmail.com>
2015-07-25 02:32:26 +0500

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -2,7 +2,7 @@
 /**
  * Open Source Social Network
  *
- * @packageOpen Source Social Network
+ * @package   Open Source Social Network
  * @author    Open Social Website Core Team <info@informatikon.com>
  * @copyright 2014 iNFORMATIKON TECHNOLOGIES
  * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
