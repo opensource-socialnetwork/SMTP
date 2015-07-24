7202a8f87b559ee84cd5b969bd02aa9e6ffa71b3
Arsalan Shah <manactric@gmail.com>
2015-07-24 14:31:06 +0500

    add settings.php

---
+++ plugins/default/settings/administrator/SMTP/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$smtp = ossn_smpt_connected(); 
+echo ossn_view_form('smtp/settings/save', array(
+    'action' => ossn_site_url() . 'action/admin/smtp/settings/save',
+    'class' => 'ossn-admin-form'	
+));
+echo "<div class='margin-top-10'><strong>", $smtp['status'],  "</strong></div>";
