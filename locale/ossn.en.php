a1b07a3bc05205defc4760e58716024d21f034ea
Arsalan Shah <manactric@gmail.com>
2015-07-24 14:30:21 +0500

    add ossn.en.php

---
+++ locale/ossn.en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+	'smtp' => 'SMTP',
+	'smtp:settings' => 'SMTP Settings',
+	'smtp:connection:connected' => 'SMTP Connection successfully established',
+	'smtp:connectio:failed' => 'SMTP Connection Failed',
+	'smtp:host' => "Mail Server (mail.yourdomain.com)",
+	'smtp:port' => 'Port',
+	'smtp:username' => 'Username (name@yourdomain.com)',
+	'smtp:password' => 'Password',
+);
+ossn_register_languages('en', $en); 
