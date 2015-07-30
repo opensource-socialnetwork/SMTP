3cd11c1df9aebecd1c8be2e3bdb65b8cfb947756
Arsalan Shah <manactric@gmail.com>
2015-07-30 10:48:51 -0700

    Update settings.php

--- plugins/default/settings/administrator/SMTP/settings.php
+++ plugins/default/settings/administrator/SMTP/settings.php
@@ -8,7 +8,7 @@
  * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
  * @link      http://www.opensource-socialnetwork.org/licence
  */
-$smtp = ossn_smpt_connected(); 
+$smtp = ossn_smtp_connected(); 
 echo ossn_view_form('smtp/settings/save', array(
     'action' => ossn_site_url() . 'action/admin/smtp/settings/save',
     'class' => 'ossn-admin-form'	
