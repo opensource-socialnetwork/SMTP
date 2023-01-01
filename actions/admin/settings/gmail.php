b3d55afa37b4d4e49faf212d9b83eb5b940514b3
Arsalan Shah <manactric@gmail.com>
2023-01-02 00:50:19 +0500

    Update gmail.php

--- actions/admin/settings/gmail.php
+++ actions/admin/settings/gmail.php
@@ -8,24 +8,25 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-require_once(__SMTP__ . 'vendor/google/vendor/autoload.php');
+require_once __SMTP__ . 'vendor/google/vendor/autoload.php';
 use League\OAuth2\Client\Provider\Google;
 
-$smtp     = new OssnComponents;
+$smtp     = new OssnComponents();
 $settings = $smtp->getSettings('SMTP');
 
-$provider = new Google([
-    'clientId' => $settings->clientId,
-    'clientSecret' => $settings->clientSecret,
-    'redirectUri' => ossn_site_url('smtp_oauth/google'),
-    'accessType' => 'offline'
-]);
-$options = [
-        'scope' => [
-                'https://mail.google.com/'
- 		]
-];
-$authUrl = $provider->getAuthorizationUrl($options);
+$provider = new Google(array(
+		'clientId'     => $settings->clientId,
+		'clientSecret' => $settings->clientSecret,
+		'redirectUri'  => ossn_site_url('smtp_oauth/google'),
+		'accessType'   => 'offline',
+));
+$options = array(
+		'prompt' => 'consent',
+		'scope'  => array(
+				'https://mail.google.com/',
+		),
+);
+$authUrl                 = $provider->getAuthorizationUrl($options);
 $_SESSION['oauth2state'] = $provider->getState();
 header("Location: {$authUrl}");
-exit;
+exit();
